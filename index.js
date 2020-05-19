d7bce0ce016cf922a9525a58e3f3029fafd1eb9a
themightyigor <vincentmarnisi@gmail.com>
2020-05-19 12:14:03 +0300

    hw-1 custom cli (#2)
    Co-authored-by: Igor Rabizov <igor_rabizov@epam.com>

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-});
-
-const port = 1337;
-server.listen(port);
-
-console.log(`Server running at http://localhost:${port}`);
+#!/usr/bin/env node
+require('./src/cli')(process.argv);
